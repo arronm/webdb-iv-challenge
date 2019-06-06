9b404eb564054994b5163f2ac6f8a81eeee4d277
Arron M <marshall.arron@gmail.com>
2019-06-06 15:52:22 -0700

    Add getRecipe to recipe-model

--- data/recipe-models.js
+++ data/recipe-models.js
@@ -1,3 +1,25 @@
 const db = require('./models')('recipe');
 
-module.exports = db;
+const getRecipe = id => {
+  return db.cb(async (db) => {
+    const ingredients = await db('recipe_ingredients as ri')
+      .join('recipe', 'recipe.id', 'ri.recipe_id')
+      .join('ingredient', 'ingredient.id', 'ri.ingredient_id')
+      .select('ingredient.name', 'ingredient.quantity', 'ingredient.unit')
+      .where('ri.id', id);
+    const recipe = await db('recipe')
+      .join('dish', 'dish.id', 'recipe.dish_id')
+      .select('dish.name as dish', 'recipe.name as recipe')
+      .where('recipe.id', id)
+      .first();
+    return {
+      ...recipe,
+      ingredients,
+    };
+  });
+}
+
+module.exports = {
+  ...db,
+  getRecipe,
+};
